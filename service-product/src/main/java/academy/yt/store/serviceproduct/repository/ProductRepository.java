985b515e0fc751c07f0800d9f860de15cb6b02b8
Daniel <daniel.blanco.herrera.contractor@bbva.com>
2022-11-02 19:18:48 -0500

    create test

---
+++ service-product/src/main/java/academy/yt/store/serviceproduct/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package academy.yt.store.serviceproduct.repository;
+
+import academy.yt.store.serviceproduct.entity.Category;
+import academy.yt.store.serviceproduct.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    public List<Product> findByCategory(Category category);
+}
